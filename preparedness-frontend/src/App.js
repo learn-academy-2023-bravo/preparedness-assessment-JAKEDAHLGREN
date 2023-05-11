5f43850005fb2ea709582e3f7ace76768096b2ad
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-05-11 11:06:56 -0700

    completed assessment

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,23 +1,45 @@
-import React from "react"
-import { Input, Label } from "reactstrap"
+import React, { useState } from "react"
+import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
-  return (
-    <div className="entire-content">
-      <h1 className="title">Preparedness Assessment</h1>
-      <div className="form">
-        <div className="input" input="text">
-          <Label for="name" className="name">Enter your name</Label>
-          <div >
-          <Input className="input-field" placeholder="type your name"/>
-        </div>
-        </div>
-        <ModalComponent />
-      </div>
-    </div>
-  )
+
+	const [input, setInput] = useState(" ")
+
+	const handleChange = (e) => {
+		setInput(e.target.value)
+	}
+  
+	const [modal, setModal] = useState(false)
+
+	const toggle = () => setModal(!modal)
+
+	const handleReset = () => {
+		setInput("")
+	}
+	return (
+		<div className="entire-content">
+			<h1 className="title">Preparedness Assessment</h1>
+			<div className="form">
+				<div className="input" input="text">
+					<Label for="name" className="input-label">
+						Enter your name
+					</Label>
+					<Input className="input-field" value={input} onChange={handleChange} />
+				</div>
+				<div className="buttons">
+					<Button className="button1" onClick={toggle}>
+						Click Me
+					</Button>
+					<Button className="button2" onClick={handleReset}>
+						Reset
+					</Button>
+					<ModalComponent toggle={toggle} input={input} modal={modal} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
